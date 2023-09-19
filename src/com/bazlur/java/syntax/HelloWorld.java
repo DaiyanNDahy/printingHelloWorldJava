843d542e4cbb5d32cfbff8baa0651283ca7fade3
DaiyanNDahy <daiyanndahy@gmail.com>
2023-09-19 19:22:15 +0600

    uploading data in one level folder

---
+++ src/com/bazlur/java/syntax/HelloWorld.java
@@ -0,0 +1,20 @@
+package com.bazlur.java.syntax;
+
+public class HelloWorld{
+
+    public static void main(String[] args){
+        
+        System.out.println("Hello, world!");
+
+        System.out.println();
+
+        System.out.print("Hello, world!");
+
+        System.out.println("Hello");
+        System.out.print(" ");
+
+        System.out.print("world!");
+        System.out.println("Hello, world!");
+    }
+}
+
